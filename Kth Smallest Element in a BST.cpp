8641b209a1e41011cccc3b9a68cfde11b83a43bd
catherinerose4711 <104436765+catherinerose4711@users.noreply.github.com>
2025-04-07 10:04:17 +0530

    Create Kth Smallest Element in a BST.cpp

---
+++ Kth Smallest Element in a BST.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int count = 0;
+    int result = -1;
+
+    void inorder(TreeNode* root, int k){
+        if(root==NULL || count >= k) return;
+
+         inorder(root->left, k);
+         
+         count++;
+         if(count==k){
+            result = root->val;
+            return;
+         }
+         inorder(root->right, k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root,k);
+        return result;
+    }
+};
